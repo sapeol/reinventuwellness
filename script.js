dedecaa9f140a50e69f4973c380f07d1bd9d867e
sapeol <sudeep.kuchara@protonmail.com>
2022-01-26 16:42:14 +0530

    -changes

---
+++ script.js
@@ -0,0 +1,45 @@
+function waitForElm(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+waitForElm(".customers-review").then((elm) => {
+  $(".customers-review").slick({
+    infinite: true,
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 1192,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
